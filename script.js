f64a72f9529444063fb807e41195fdc5a89b4733
Fumike <iribirimary@gmail.com>
2020-03-26 22:54:39 +0100

    First Javascript Task

---
+++ script.js
@@ -0,0 +1,9 @@
+var myName = "Mary";			//variable
+var courses =["Html", "CSS","Javascript","PHP","Python","Nodejs","GO","Ruby"];		//array
+
+console.log(myName, courses);  //output name and array
+
+
+for (let i = 1; i <= 200; i++) {
+	if (i % 2 === 0) console.log(i);
+}
